448231b9bf07ef337aad2a08d90252f3cb9eab67
2jacks <anton.nozdrin.21@gmail.com>
2022-08-16 12:28:25 +0500

    test gh deploy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export const App = () => {
 	return (
 		<div className='app'>
-			<h1>We aint gonna take</h1>
+			<h1>We aint gonna take anymore</h1>
 		</div>
 	)
 }
